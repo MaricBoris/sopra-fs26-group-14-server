f4fbb828dae2ccd4e519c09739760d06db7ab72c
Mohammadsadra Karimian <karimian.sadra.mohammad@gmail.com>
2026-03-24 17:43:30 +0100

    #215, #39, #43: DTOMapperTest cases adjusted and added for the refined User Entity, UserPostDTO and the new UserPersonalGetDTO.

--- src/test/java/ch/uzh/ifi/hase/soprafs26/rest/mapper/DTOMapperTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs26/rest/mapper/DTOMapperTest.java
@@ -1,45 +1,66 @@
 package ch.uzh.ifi.hase.soprafs26.rest.mapper;
 
 import org.junit.jupiter.api.Test;
-
-import ch.uzh.ifi.hase.soprafs26.constant.UserStatus;
 import ch.uzh.ifi.hase.soprafs26.entity.User;
 import ch.uzh.ifi.hase.soprafs26.rest.dto.UserGetDTO;
 import ch.uzh.ifi.hase.soprafs26.rest.dto.UserPostDTO;
+import ch.uzh.ifi.hase.soprafs26.rest.dto.UserPersonalGetDTO;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * DTOMapperTest
- * Tests if the mapping between the internal and the external/API representation
- * works.
+ * Verifies that MapStruct correctly translates between the internal Entity
+ * and the various DTO representations used by the Controller.
  */
 public class DTOMapperTest {
-	@Test
-	public void testCreateUser_fromUserPostDTO_toUser_success() {
-		// create UserPostDTO
-		UserPostDTO userPostDTO = new UserPostDTO();
-		userPostDTO.setUsername("username");
 
-		// MAP -> Create user
-		User user = DTOMapper.INSTANCE.convertUserPostDTOtoEntity(userPostDTO);
+    @Test
+    public void testCreateUser_fromUserPostDTO_toUser_success() {
+        // create UserPostDTO
+        UserPostDTO userPostDTO = new UserPostDTO();
+        userPostDTO.setUsername("testUsername");
+        userPostDTO.setPassword("testPassword123");
 
-		// check content
-		assertEquals(userPostDTO.getUsername(), user.getUsername());
-	}
+        // MAP -> Create user
+        User user = DTOMapper.INSTANCE.convertUserPostDTOtoEntity(userPostDTO);
 
-	@Test
-	public void testGetUser_fromUser_toUserGetDTO_success() {
-		// create User
-		User user = new User();
-		user.setUsername("firstname@lastname");
-		user.setToken("1");
+        // check content
+        assertEquals(userPostDTO.getUsername(), user.getUsername());
+        assertEquals(userPostDTO.getPassword(), user.getPassword());
+    }
 
-		// MAP -> Create UserGetDTO
-		UserGetDTO userGetDTO = DTOMapper.INSTANCE.convertEntityToUserGetDTO(user);
+    @Test
+    public void testGetUser_fromUser_toUserGetDTO_success() {
+        // create User
+        User user = new User();
+        user.setId(1L);
+        user.setUsername("publicUser");
 
-		// check content
-		assertEquals(user.getId(), userGetDTO.getId());
-		assertEquals(user.getUsername(), userGetDTO.getUsername());
-	}
+        // MAP -> Create UserGetDTO
+        UserGetDTO userGetDTO = DTOMapper.INSTANCE.convertEntityToUserGetDTO(user);
+
+        // check content
+        assertEquals(user.getId(), userGetDTO.getId());
+        assertEquals(user.getUsername(), userGetDTO.getUsername());
+    }
+
+    @Test
+    public void testGetPersonalUser_fromUser_toUserPersonalGetDTO_success() {
+        // create User with full details
+        User user = new User();
+        user.setId(1L);
+        user.setUsername("privateUser");
+        user.setToken("secret-uuid-token");
+        user.setBio("This is my private bio.");
+
+        // MAP -> Create UserPersonalGetDTO
+        UserPersonalGetDTO personalDTO = DTOMapper.INSTANCE.convertEntityToUserPersonalGetDTO(user);
+
+        // check content - specifically the sensitive/extra fields
+        assertEquals(user.getId(), personalDTO.getId());
+        assertEquals(user.getUsername(), personalDTO.getUsername());
+        assertEquals(user.getToken(), personalDTO.getToken());
+        assertEquals(user.getBio(), personalDTO.getBio());
+    }
 }
