381b8d2216e041df2a75350e55f33c43aeef3aea
Mohammadsadra Karimian <karimian.sadra.mohammad@gmail.com>
2026-03-26 12:40:08 +0100

    #39, #43, #49: Changed the fields of the two GET DTOs.

--- src/main/java/ch/uzh/ifi/hase/soprafs26/rest/dto/UserPersonalGetDTO.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/rest/dto/UserPersonalGetDTO.java
@@ -1,11 +1,14 @@
 package ch.uzh.ifi.hase.soprafs26.rest.dto;
 
+import java.util.Date;
+
 public class UserPersonalGetDTO {
 
     private Long id;
     private String username;
     private String bio;
     private String token;
+    private Date creationDate;
     //private History history;
 
     public Long getId() { return id; }
@@ -20,6 +23,11 @@ public class UserPersonalGetDTO {
 
     public String getBio() { return bio; }
     public void setBio(String bio) { this.bio = bio; }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+    public void setCreationDate(Date creationDate) { this.creationDate = creationDate; }
 
     public String getToken() { return token; }
     public void setToken(String token) { this.token = token; }
